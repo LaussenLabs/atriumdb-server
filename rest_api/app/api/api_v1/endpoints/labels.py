c4880e9bce4ea59dc9982a5faf52a888cd6a6922
svecile <45304049+svecile@users.noreply.github.com>
2024-01-29 16:23:38 -0500

    change labels search endpoint to a post request. Also add endpoints for turning label name id's into names and vice versa and also the same thing for label sources

--- rest_api/app/api/api_v1/endpoints/labels.py
+++ rest_api/app/api/api_v1/endpoints/labels.py
@@ -8,39 +8,30 @@ router = APIRouter()
 
 
 # @router.get("/", dependencies=[Depends(auth.implicit_scheme)], response_model=Dict[int, schemas.Label])
-@router.get("/", response_model=List[schemas.Label])
-async def search_labels(
-        limit: int = 1000,
-        offset: int = 0,
-        label_name_id_list: Optional[List[int]] = None,
-        name_list: Optional[List[str]] = None,
-        device_list: Optional[List[Union[int, str]]] = None,
-        patient_id_list: Optional[List[int]] = None,
-        start_time: Optional[int] = None,
-        end_time: Optional[int] = None,
-        time_units: Optional[str] = None,):
+@router.post("/", response_model=List[schemas.Label])
+async def search_labels(body: schemas.LabelsQuery):
         #user: Auth0User = Security(auth.get_user)):
 
     # make sure they arnt asking for too much data at a time
-    if limit > 1000:
+    if body.limit > 1000:
         raise HTTPException(status_code=400, detail="Limits of greater than 1000 are not allowed.")
 
-    if label_name_id_list and name_list:
+    if body.label_name_id_list and body.name_list:
         raise HTTPException(status_code=400, detail="Only one of label_name_id_list or name_list should be provided.")
 
-    if device_list and patient_id_list:
+    if body.device_list and body.patient_id_list:
         raise HTTPException(status_code=400, detail="Only one of device_list or patient_id_list should be provided.")
 
     try:
         labels = atriumdb_sdk.get_labels(
-            label_name_id_list=label_name_id_list,
-            name_list=name_list,
-            device_list=device_list,
-            patient_id_list=patient_id_list,
-            start_time=start_time,
-            end_time=end_time,
-            time_units=time_units,
-            limit=limit, offset=offset
+            label_name_id_list=body.label_name_id_list,
+            name_list=body.name_list,
+            device_list=body.device_list,
+            patient_id_list=body.patient_id_list,
+            start_time=body.start_time,
+            end_time=body.end_time,
+            time_units=body.time_units,
+            limit=body.limit, offset=body.offset
         )
     except ValueError as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -49,6 +40,33 @@ async def search_labels(
 
 
 # @router.get("/names", dependencies=[Depends(auth.implicit_scheme)], response_model=Dict[int, str])
+@router.get("/name", response_model=Dict[int, schemas.LabelName])
+async def get_all_label_names(label_name_id: int = None, label_name: str = None):#, user: Auth0User = Security(auth.get_user)):
+
+    if label_name_id and label_name:
+        raise HTTPException(status_code=400, detail="Only one of label_name_id or label_name should be provided.")
+
+    if label_name_id is not None:
+        return atriumdb_sdk.get_label_name_info(label_name_id=label_name_id)
+    if label_name is not None:
+        return atriumdb_sdk.get_label_name_id(name=label_name)
+
+
+# @router.get("/names", dependencies=[Depends(auth.implicit_scheme)], response_model=Dict[int, str])
 @router.get("/names", response_model=Dict[int, schemas.LabelName])
 async def get_all_label_names(limit: int = 1000, offset: int = 0):#, user: Auth0User = Security(auth.get_user)):
     return atriumdb_sdk.get_all_label_names(limit=limit, offset=offset)
+
+
+# @router.get("/names", dependencies=[Depends(auth.implicit_scheme)], response_model=Dict[int, str])
+@router.get("/source", response_model=Dict[int, schemas.LabelName])
+async def get_all_label_names(label_source_id: int = None, label_source_name: str = None):#, user: Auth0User = Security(auth.get_user)):
+
+    if label_source_id and label_source_name:
+        raise HTTPException(status_code=400, detail="Only one of label_source_id or label_source_name should be provided.")
+
+    if label_source_id is not None:
+        return atriumdb_sdk.get_label_source_info(label_source_id=label_source_id)
+    if label_source_name is not None:
+        return atriumdb_sdk.get_label_source_id(name=label_source_name)
+
--- rest_api/app/schemas/label.py
+++ rest_api/app/schemas/label.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 
 class LabelName(BaseModel):
@@ -19,3 +19,15 @@ class Label(BaseModel):
     end_time_n: int
     label_source_id: Optional[int] = None
     label_source: Optional[str] = None
+
+
+class LabelsQuery(BaseModel):
+    limit: Optional[int] = 1000,
+    offset: Optional[int] = 0,
+    label_name_id_list: Optional[List[int]] = None,
+    name_list: Optional[List[str]] = None,
+    device_list: Optional[List[int | str]] = None,
+    patient_id_list: Optional[List[int]] = None,
+    start_time: Optional[int] = None,
+    end_time: Optional[int] = None,
+    time_units: Optional[str] = None
