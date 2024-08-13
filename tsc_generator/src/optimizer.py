fdbbb80ab0aaffc6ec399a579d84649a90f4bf6f
svecile <45304049+svecile@users.noreply.github.com>
2024-08-13 17:05:08 -0400

    - update code so there is a hard limit on how many blocks can be optimized per run - update delete tsc file code to work with paths to stop bug with hidden folders

--- tsc_generator/src/optimizer.py
+++ tsc_generator/src/optimizer.py
@@ -1,15 +1,17 @@
 import math
-import os
 import logging
 import time
 import xxhash
 from config import config
 from atriumdb import AtriumSDK, adb_functions
 from helpers import sql_functions
+from pathlib import Path
 
+# the max number of blocks to optimize for a run
+MAX_BLOCKS_PER_RUN = 100_000
+sdk = None
 
 _LOGGER = logging.getLogger(__name__)
-sdk = None
 
 
 def merge_small_tsc_files(device_id, measure_id):
@@ -24,12 +26,34 @@ def merge_small_tsc_files(device_id, measure_id):
     # get blocks from tsc files that are not big enough
     block_list = sql_functions.find_small_tsc_files(sdk=sdk, device_id=device_id, measure_id=measure_id,
                                                     target_tsc_file_size=config.svc_tsc_gen['target_tsc_file_size'])
-    _LOGGER.info(f"Finding small tsc files took {time.perf_counter()-tik} s, for device_id={device_id}, measure_id={measure_id}")
+    _LOGGER.info(f"Finding small tsc files took {time.perf_counter() - tik} s, for device_id={device_id}, measure_id={measure_id}")
 
     # if there is only one tsc file then don't optimize since there is only one partly full tsc file
     num_tsc_fles = len(set([block[3] for block in block_list]))
     if num_tsc_fles < 2:
         return 0
+
+    tik = time.perf_counter()
+    # we need to make sure that 100_000 blocks is not smaller than the target_tsc_file_size because if it is than
+    # the optimizer will never reach the desired file size and will get stuck optimizing the same data over and over
+    bytes_total, idx = 0, 0
+    for i, block in enumerate(block_list):
+        bytes_total += block[5]
+
+        # if the sum is greater than the file size than break
+        if bytes_total >= config.config.svc_tsc_gen['target_tsc_file_size']:
+            idx = i
+            break
+
+    # if it took less than 100_000 blocks to fill a file than use the default of 100_000 blocks for the optimization
+    # if it took more than however many it took will be used to slice the block list
+    if idx < MAX_BLOCKS_PER_RUN:
+        idx = MAX_BLOCKS_PER_RUN
+
+    # limit the number of blocks that can be optimized during a single run
+    block_list = block_list[:idx]
+
+    _LOGGER.info(f"Limit to 100,000 blocks took {time.perf_counter() - tik} s, for device_id={device_id}, measure_id={measure_id}")
 
     tik = time.perf_counter()
     # checksum data to ensure before and after data are the same
@@ -61,12 +85,11 @@ def merge_small_tsc_files(device_id, measure_id):
         # insert the new filenames and their associated blocks. Then delete the old blocks in one transaction
         sql_functions.update_block_tsc_data(sdk, filenames, block_list, block_batch_slices, start_byte_array)
 
-        _LOGGER.info(f"Merging tsc files took {time.perf_counter()-tik} s, for device_id={device_id}, measure_id={measure_id}")
-
-        # get the new blocks by using the tsc filenames
-        new_blocks = sql_functions.select_blocks_by_file(sdk, filenames)
+        _LOGGER.info(f"Merging tsc files took {time.perf_counter() - tik} s, for device_id={device_id}, measure_id={measure_id}")
 
         tik = time.perf_counter()
+        # get the new blocks by using the tsc filenames
+        new_blocks = sql_functions.select_blocks_by_file(sdk, filenames)
         # checksum the new blocks
         checksum_after = checksum_data(sdk, new_blocks)
 
@@ -90,14 +113,13 @@ def make_optimal_tsc_files(block_list):
 
     for i, block in enumerate(block_list):
 
-        start_byte_array.append(end_byte-start_byte)
+        start_byte_array.append(end_byte - start_byte)
         end_byte += block[5]
 
         # here we are cutting the bytes stream into >= the target file size
         # if we go over the target tsc file size that's okay, but we must not be under the target size or the
         # optimizer will try to optimize this file again on the next optimizer run
-        if end_byte >= config.svc_tsc_gen['target_tsc_file_size'] or i+1 == len(block_list):
-
+        if end_byte >= config.svc_tsc_gen['target_tsc_file_size'] or i + 1 == len(block_list):
             # append the numbers needed to slice the old block list later for reading the encoded bytes in batches
             block_batch_slices.append((block_start, i + 1))
 
@@ -110,14 +132,12 @@ def make_optimal_tsc_files(block_list):
 
 # This function is used to confirm that the data before the optimization is the same as the data after the optimization
 def checksum_data(sdk, block_list):
-
     num_chunks = math.ceil(len(block_list) / config.svc_tsc_gen['num_blocks_checksum'])
     checksum = xxhash.xxh3_128()
 
     for i in range(num_chunks):
-
         # make read list as small as possible to speed up process
-        read_list = adb_functions.condense_byte_read_list(block_list[i*config.svc_tsc_gen['num_blocks_checksum']:(i+1)*config.svc_tsc_gen['num_blocks_checksum']])
+        read_list = adb_functions.condense_byte_read_list(block_list[i * config.svc_tsc_gen['num_blocks_checksum']:(i + 1) * config.svc_tsc_gen['num_blocks_checksum']])
         # extract file ids from condensed read list and get the tsc file names they map to
         file_id_list = [row[2] for row in read_list]
         filename_dict = sdk.get_filename_dict(file_id_list)
@@ -133,32 +153,22 @@ def checksum_data(sdk, block_list):
 def delete_unreferenced_tsc_files(sdk):
     _LOGGER.info("Starting removal of unreferenced tsc files")
     # find tsc files in the file_index that have no references to them in the block_index
-    files = sql_functions.find_unreferenced_tsc_files(sdk)
+    file_info = sql_functions.find_unreferenced_tsc_files(sdk)
 
-    if len(files) == 0:
+    if len(file_info) == 0:
         _LOGGER.info("No unreferenced tsc files to remove")
         return
 
-    # extract file names from files and make it a set so we can do a set intersection later
-    file_names = {file[1] for file in files}
     # extract the ids and put them in a tuple so we can remove them from the sql table later
-    file_ids = [(file[0],) for file in files]
+    file_ids = [(file[0],) for file in file_info]
+
+    for file in file_info:
+        # make a path using device_id, measure_id, and file_name from the returned sql row and delete the file
+        (Path(sdk.file_api.top_level_dir) / file[1] / file[2] / file[3]).unlink(missing_ok=True)
+        _LOGGER.info(f"Deleting tsc file {file[3]} from disk")
 
     # free up memory
-    del files
-
-    # walk the tsc directory looking for files to delete (os.walk is a generator for memory efficiency)
-    for root, _, files in os.walk(sdk.file_api.top_level_dir):
-        # check if there is a match between any of the tsc file names to be deleted and files in the current directory
-        matches = set(files) & file_names
-
-        # if you find a match remove the file from disk
-        for m in matches:
-            _LOGGER.info(f"Deleting tsc file {m} from disk")
-            os.remove(os.path.join(root, m))
-
-    # free up memory
-    del file_names
+    del file_info
 
     # remove them from the file_index
     sql_functions.delete_tsc_files(sdk, file_ids)
