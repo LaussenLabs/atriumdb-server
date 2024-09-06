2d5ae7ac38dec658380571e5723c31a2a3721fbf
svecile <spencervecile@gmail.com>
2024-09-06 10:49:36 -0400

    - remove unused function - update default wal file timeout

--- tsc_generator/src/helpers/sql_functions.py
+++ tsc_generator/src/helpers/sql_functions.py
@@ -10,15 +10,6 @@ def find_unreferenced_tsc_files(sdk):
         return cursor.fetchall()
 
 
-# def find_devices_measures_with_small_tsc_files(sdk, target_tsc_file_size):
-#     with sdk.sql_handler.connection() as (conn, cursor):
-#         cursor.execute("SELECT measure_id, device_id FROM block_index WHERE file_id IN "
-#                        "(SELECT file_id FROM block_index GROUP BY file_id HAVING SUM(num_bytes) < ?)"
-#                        " GROUP BY measure_id, device_id HAVING COUNT(DISTINCT file_id) >= 2",
-#                        (target_tsc_file_size,))
-#         return cursor.fetchall()
-
-
 def find_devices_measures_with_small_tsc_files(sdk, target_tsc_file_size):
     with sdk.sql_handler.connection() as (conn, cursor):
         cursor.execute("SELECT bi1.measure_id, bi1.device_id FROM block_index bi1 JOIN "
